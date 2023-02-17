8b06491f247ed88fca21690b2b95fa2020f31142
han hyunhee <heeyacjstk2@gmail.com>
2023-02-17 18:10:42 +0900

    [test]2

--- client/src/pages/Questions.js
+++ client/src/pages/Questions.js
@@ -2,7 +2,7 @@ export default function Questions() {
   return (
     <div>
       <div>Questions</div>
-      <Div className="test">test</Div>
+      <Div className="test">test1</Div>
     </div>
   );
 }
