ec9094885ee0e235c8b97e0b225bd02d1e8e9f23
han hyunhee <heeyacjstk2@gmail.com>
2023-02-23 00:24:25 +0900

    [design]questions,askquestions

--- client/src/pages/Questions.js
+++ client/src/pages/Questions.js
@@ -1,66 +1,77 @@
 import styled from 'styled-components';
 import Dummyq from './dummyq';
 import { Link } from 'react-router-dom';
-const Qmain = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 70%;
-  height: 80%;
-  .qcss {
-    margin-top: 20px;
-    font-size: 35px;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-  }
-  .askbutton {
-    font-size: 14px;
-    background: black;
-    color: white;
-    width: 12vw;
-    height: 5vh;
-    border-radius: 5px;
-  }
-  .sort {
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-  }
-  .ql {
-    display: flex;
-    flex-direction: row;
-  }
-  .ql2 {
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    border: 2px solid black;
-    border-radius: 4px;
-  }
-  li {
-    list-style: none;
-  }
 
-  .li1 {
-    border-right: 2px solid black;
-    padding: 8px;
-  }
-  .li2 {
-    padding: 8px;
-  }
-  .filter {
-    background: lightgrey;
-    margin: 10px;
-    border-radius: 3px;
-    .leftli {
-    }
-  }
-`;
 export default function Questions() {
+  const Qmain = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 80%;
+    .qcss {
+      margin-top: 20px;
+
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+    }
+    .askbutton {
+      font-size: 14px;
+      background: #0d0d0e;
+      color: #f6f6f6;
+      width: 100px;
+      height: 5vh;
+      border-radius: 5px;
+    }
+    .sort {
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+    }
+    .ql {
+      display: flex;
+      flex-direction: row;
+    }
+    ul {
+      list-style: none;
+      padding-left: 0px;
+      display: flex;
+      flex-direction: row;
+
+      border: 2px solid black;
+      border-radius: 4px;
+      background: #ffffff;
+    }
+
+    li {
+      list-style: none;
+    }
+
+    .li1 {
+      border-right: 2px solid black;
+      padding: 8px;
+    }
+    .rightli {
+      border-right: none;
+    }
+    .filter {
+      background: lightgrey;
+      width: 57px;
+      height: 33px;
+      margin: 10px;
+      border-radius: 3px;
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: center;
+      .leftli {
+      }
+    }
+  `;
   return (
     <Qmain>
       <div className="qcss">
-        <div>All Questions</div>
+        <h1>All Questions</h1>
         <button className="askbutton">
           <Link to="/askquestion">AskQuestion</Link>
         </button>
@@ -68,14 +79,17 @@ export default function Questions() {
       <div className="sort">
         <h3>48,296 questions</h3>
         <nav className="ql">
-          <ul className="ql2">
+          <ul>
             <li className="li1 leftli">Newest</li>
             <li className="li1">Active</li>
             <li className="li1">Unanswered</li>
             <li className="li1">Frequent</li>
-            <li className="li2">More</li>
+            <li className="li1 rightli">More</li>
           </ul>
-          <button className="filter">Filter</button>
+          <button className="filter">
+            <img className="filterimg" src="filter.png" alt="filter(1).png" />
+            Filter
+          </button>
         </nav>
       </div>
 
