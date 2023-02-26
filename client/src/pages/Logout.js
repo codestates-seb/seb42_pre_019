17e079f8426711d959ed7adaef773498132a3fde
ssomni <ssm9991@gmail.com>
2023-02-27 04:47:53 +0900

    [feat]logout 기능구현

---
+++ client/src/pages/Logout.js
@@ -0,0 +1,62 @@
+import styled from 'styled-components';
+import '../App.css';
+const Logoutcss = styled.div`
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  right: 12px;
+  top: 48px;
+  width: 300px;
+  height: 100px;
+  background-color: white;
+  box-shadow: 0 1px 2px hsla(0, 0%, 0%, 0.05), 0 1px 4px hsla(0, 0%, 0%, 0.05),
+    0 2px 8px hsla(0, 0%, 0%, 0.05);
+  background-color: hsl(210, 8%, 97.5%);
+  button {
+    position: relative;
+    align-items: center;
+    background-color: hsl(205, 46%, 92%);
+    border: 1px solid hsl(205, 47%, 42%);
+    border-color: hsl(205, 41%, 63%);
+    border-radius: 3px;
+    box-shadow: inset 0 1px 0 0 hsla(0, 0%, 100%, 0.7);
+    color: hsl(205, 47%, 42%);
+    padding: 0.8em;
+    line-height: 13px;
+    font-size: 13px;
+    margin-top: 1px;
+    text-decoration: none;
+    cursor: pointer;
+  }
+  .logOut {
+    background-color: hsl(206, 100%, 52%);
+    color: white;
+    text-align: center;
+    margin: 0px;
+    font-weight: 400;
+    padding-top: 7px;
+    padding-bottom: 7px;
+    width: 100px;
+  }
+  .logOut:hover {
+    background-color: hsl(206, 100%, 40%);
+  }
+`;
+
+export default function Logout(props, { outPage }) {
+  const handleOut = () => {
+    // props.setLogoutBtn(!props.logoutBtn);
+    // setProps(!props);
+    console.log(props);
+    console.log(outPage);
+    // setIsLogin(!isLogin.isLogin);
+  };
+  return (
+    <Logoutcss>
+      <button className="logOut" onClick={handleOut}>
+        Log out
+      </button>
+    </Logoutcss>
+  );
+}
